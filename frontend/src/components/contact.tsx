6065bde5166ed09891921a4da252e71a0a4d569e
SyrineGhl <syrine.ghoul@gmail.com>
2023-07-24 17:00:34 +0200

    add index+ app

---
+++ frontend/src/components/contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact: React.FC = () => {
+  return (
+    <section id="contact">
+      <h2>Contacte-moi</h2>
+      {/* Ajoute ici un formulaire de contact ou simplement tes coordonnées de contact. */}
+    </section>
+  );
+};
+
+export default Contact;
---
+++ frontend/src/components/project.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects: React.FC = () => {
+  return (
+    <section id="projects">
+      <h2>Mes Projets</h2>
+      {/* Écris ici la liste de tes projets avec des liens vers leur démo ou leur code source. */}
+    </section>
+  );
+};
+
+export default Projects;
