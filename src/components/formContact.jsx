d67c5839e99599b304268295b3d848b4fd7f665c
SyrineGhl <syrine.ghoul@gmail.com>
2023-08-08 17:16:47 +0200

    add contact

---
+++ src/components/formContact.jsx
@@ -0,0 +1,49 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+ const FormContact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <form className="contact-form-page" ref={form} onSubmit={sendEmail}>
+      <div className="form-group-page">
+        <label htmlFor="email">EMAIL</label>
+        <input type="email" id="email" name="from_email" required />
+      </div>
+      <div className="form-group-page">
+        <label htmlFor="subject">OBJET</label>
+        <input type="text" id="subject" name="from_subject" required />
+      </div>
+      <div className="form-group-page">
+        <label htmlFor="message">MESSAGE</label>
+        <textarea id="message" name="message" required></textarea>
+      </div>
+      <div className="form-group-button">
+        <button className="button-contact" type="submit">
+          ENVOYER
+        </button>
+      </div>
+    </form>
+  );
+};
+export default FormContact;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,8 +2,10 @@ import React from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import contactImage from "../assets/images/contact.jpg"; 
+import FormContact from "../components/formContact";
 
 const Contact = () => {
+  
   return (
     <>
     <Header/>
@@ -14,29 +16,7 @@ const Contact = () => {
         <div className="contact-image">
           <img src={contactImage} alt="Contact" />
                   </div>
-        <div className="contact-form-page">
-          <form>
-            <div className="form-group-page">
-              <label htmlFor="name">NOM</label>
-              <input type="text" id="name" name="name" required />
-            </div>
-            <div className="form-group-page">
-              <label htmlFor="email">EMAIL</label>
-              <input type="email" id="email" name="email" required />
-            </div>
-            <div className="form-group-page">
-              <label htmlFor="subject">OBJET</label>
-              <input type="text" id="subject" name="subject" required />
-            </div>
-            <div className="form-group-page">
-              <label htmlFor="message">MESSAGE</label>
-              <textarea id="message" name="message" required></textarea>
-            </div>
-            <div className="form-group-button">
-              <button className="button-contact" type="submit">ENVOYER</button>
-            </div>
-          </form>
-        </div>
+         <FormContact/>
       </div>
     </div>
     <Footer />
