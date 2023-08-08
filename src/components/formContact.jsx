08c6ed3ffb3810533de7893409b4ee8e85f5f1e6
SyrineGhl <syrine.ghoul@gmail.com>
2023-08-08 23:06:06 +0200

    fix bug responsive

--- src/components/formContact.jsx
+++ src/components/formContact.jsx
@@ -28,11 +28,11 @@ import emailjs from "@emailjs/browser";
     <form className="contact-form-page" ref={form} onSubmit={sendEmail}>
       <div className="form-group-page">
         <label htmlFor="email">EMAIL</label>
-        <input type="email" id="email" name="from_email" autocomplete="off" required />
+        <input type="email" id="email" name="from_email" autoComplete="off" required />
       </div>
       <div className="form-group-page">
         <label htmlFor="subject">OBJET</label>
-        <input type="text" id="subject" name="from_subject" autocomplete="off"required />
+        <input type="text" id="subject" name="from_subject" autoComplete="off"required />
       </div>
       <div className="form-group-page">
         <label htmlFor="message">MESSAGE</label>
